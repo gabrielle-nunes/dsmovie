968e5b284e5d2f2a5e5652b3ae2153f5700c0f51
Gabrielle <gabinunes09@gmail.com>
2022-01-11 23:58:59 -0300

    movie card

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,10 +1,33 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
+function Listing() {
+  return (
+    <>
+      <Pagination />
+      <div className="container">
+        <div className="row">
 
-function Listing (){
-    return(
-        <Pagination/>
-    )
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Listing;
